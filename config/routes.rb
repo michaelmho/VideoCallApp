48b965d7fad2fdc37153ac62b033b57592cfdf0d
michaelmho <michaelomordia.mo@gmail.com>
2021-06-19 17:01:22 -0500

    implemented devise sign up and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'home#index'
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'register', to: 'devise/registrations#new'
+    delete 'logout', to: 'sessions#destroy'
+  end
 end
