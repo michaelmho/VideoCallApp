fc864fc99d7f89115ee088b694fce132f542a0de
michaelmho <michaelomordia.mo@gmail.com>
2021-06-22 11:05:57 -0500

    trying to fix go online problem

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -1,8 +1,5 @@
 import roomSubscriber from "../channels/room_channel";
 
-/*  listening for changes to the input field, if a file is
-    selected we read it using the FileReader api, then we
-    change the src attr of the image tag by the file url */
 document.addEventListener("DOMContentLoaded", (event) => {
   /* Avatar upload functionality for user registration */
   var readURL = function (input) {
@@ -17,6 +14,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
   };
 
+  $(".file-upload").on("change", function () {
+    readURL(this);
+  });
+
+  $(".upload-button").on("click", function () {
+    $(".file-upload").click();
+  });
+
   /* Scroll to the developers section when the start button is clicked */
   const startBtn = document.getElementById("js-start-btn");
   const devSection = document.getElementById("js-developers");
@@ -27,15 +32,49 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
   }
 
-  $(".file-upload").on("change", function () {
-    readURL(this);
-  });
+  /* Change the background color and innerHTML when the online button is hovered */
+  const onlineBtn = document.getElementById("js-onlinebtn");
+  if (onlineBtn !== null) {
+    onlineBtn.addEventListener("mouseover", (event) => {
+      onlineBtn.style.backgroundColor = "red";
+      onlineBtn.style.borderColor = "red";
+      onlineBtn.innerHTML = "Go offline";
+    });
 
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-  });
+    onlineBtn.addEventListener("mouseout", (event) => {
+      onlineBtn.style.backgroundColor = "#3DC794";
+      onlineBtn.style.borderColor = "#3DC794";
+      onlineBtn.innerHTML = "You are online";
+    });
+  }
 
-  $(".upload-button").on("click", function () {
-    $(".file-upload").click();
+  var cameraIcons = document.getElementsByClassName("camera-icon");
+  for (let item of cameraIcons) {
+    item.addEventListener("click", (event) => {
+      event.preventDefault();
+      var recipient_id = item.getAttribute("data-id");
+      var recipient_name = item.getAttribute("data-name");
+      console.log(recipient_id);
+      console.log(recipient_name);
+      var recipient_name_modal = document.getElementById(
+        "recipient_name_modal"
+      );
+      recipient_name_modal.innerHTML = recipient_name;
+      $("#sender-notif-modal").modal("show");
+
+      roomSubscriber.call(recipient_id);
+    });
+  }
+
+  // Call the answer method when the answer_btn is clicked.
+  const answerBtn = document.getElementById("answer-call");
+  answerBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    var session_id = document.getElementById("session_id").innerHTML;
+    var sender_id = document.getElementById("sender_id").innerHTML;
+    console.log(session_id);
+    console.log("answer btn clicked");
+    $("#receiver-notif-modal").modal("hide");
+    roomSubscriber.answer(session_id, sender_id);
   });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
   root to: 'home#index'
 
+  post 'online', to: 'home#online'
+  post 'offline', to: 'home#offline'
+
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'register', to: 'devise/registrations#new'
     get 'logout', to: 'sessions#destroy'
   end
-
-  post 'online', to: 'home#online'
-  post 'offline', to: 'home#offline'
 end
